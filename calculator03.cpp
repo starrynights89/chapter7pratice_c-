272c82af92d70461dd8c66b71205117d9066afa0
starrynights89 <alexander-hartson@live.com>
2017-11-17 13:42:01 -0600

    bug fix

--- calculator03.cpp
+++ calculator03.cpp
@@ -209,7 +209,7 @@ Token Token_stream::get() //read a token from cin and compose a Token
 				{
 					return Token(let); //declaration keywordS
 				}
-				return Token(name, s);
+				return Token{name, s};
 			} 
 			error("Bad token"); 
 	}
